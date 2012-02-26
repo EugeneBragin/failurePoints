85d6d124894390e777762b8aa660962a433ee4f1
Eugene Bragin <>
2012-02-26 19:46:51 +0000

    initial commit

---
+++ src/failurePoints/plugin.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package failurePoints;
+
+/**
+ *
+ * @author eb4
+ */
+public class plugin {
+  
+}
