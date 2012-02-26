84927fe7ffa18ee9784028a8dfd082b7a3419293
Eugene Bragin <>
2012-02-26 21:23:20 +0000

    first menu entry

--- src/failurePoints/plugin.java
+++ src/failurePoints/plugin.java
@@ -4,10 +4,38 @@
  */
 package failurePoints;
 
+
+import java.awt.event.ActionEvent;
+
+import cytoscape.Cytoscape;
+import cytoscape.view.CyMenus;
+import cytoscape.plugin.CytoscapePlugin;
+import cytoscape.util.CytoscapeAction;
+
+
 /**
  *
  * @author eb4
  */
-public class plugin {
+public class plugin extends CytoscapePlugin {
+
+  public plugin() {
+    selectFailurePointsAction selectAction = new selectFailurePointsAction(this);
+    Cytoscape.getDesktop().getCyMenus().addCytoscapeAction((CytoscapeAction)selectAction);
+  }
   
+
+  public class selectFailurePointsAction extends CytoscapeAction {
+
+    public selectFailurePointsAction(plugin myPlugin) {
+      super("Select Failure Points");
+      setPreferredMenu("Plugins.Failure Points");
+    }
+
+    public void actionPerformed(ActionEvent e) {
+    }
+
+  }  
+  
+
 }
