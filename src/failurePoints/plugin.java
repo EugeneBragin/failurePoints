35f1e42a3371209c5690397013b7e73ceec50c9e
Eugene Bragin <eugene.bragin@gmail.com>
2012-02-27 16:52:18 +0000

    hasPathAvoiding certain node implemented

--- src/failurePoints/plugin.java
+++ src/failurePoints/plugin.java
@@ -5,12 +5,15 @@
 package failurePoints;
 
 
+import cytoscape.CyNetwork;
 import java.awt.event.ActionEvent;
 
 import cytoscape.Cytoscape;
 import cytoscape.view.CyMenus;
 import cytoscape.plugin.CytoscapePlugin;
 import cytoscape.util.CytoscapeAction;
+import cytoscape.view.CyNetworkView;
+import javax.swing.JOptionPane;
 
 
 /**
@@ -20,8 +23,9 @@ import cytoscape.util.CytoscapeAction;
 public class plugin extends CytoscapePlugin {
 
   public plugin() {
-    highlightFailurePointsAction highlightAction = new highlightFailurePointsAction(this);
-    Cytoscape.getDesktop().getCyMenus().addCytoscapeAction((CytoscapeAction)highlightAction);
+    Cytoscape.getDesktop().getCyMenus().addCytoscapeAction(new highlightFailurePointsAction(this));
+    Cytoscape.getDesktop().getCyMenus().addCytoscapeAction(new hasPathAction(this));
+    Cytoscape.getDesktop().getCyMenus().addCytoscapeAction(new hasPathAvoidingAction(this));
   }
   
 
@@ -34,7 +38,60 @@ public class plugin extends CytoscapePlugin {
 
     public void actionPerformed(ActionEvent e) {
     }
+  }  
+  
+  public class hasPathAction extends CytoscapeAction {
 
+    CyNetwork network;
+    CyNetworkView view;    
+
+    public hasPathAction(plugin myPlugin) {
+      super("Has path");
+      setPreferredMenu("Plugins.Failure Points");
+    }
+
+    public void actionPerformed(ActionEvent e) {
+      view = Cytoscape.getCurrentNetworkView();
+      network = Cytoscape.getCurrentNetwork();
+      
+      if (view.getSelectedNodeIndices().length != 2) {
+          JOptionPane.showMessageDialog(view.getComponent(), "Please select 2 nodes");
+      } else {
+          int selectedNodes[] = view.getSelectedNodeIndices();
+          //calling the method hasPath (implemented below) to see if there is a path
+          //between selectedNode[0] and selectedNodes[1], then print the result to the
+          //command line
+          boolean isConnected = util.hasPath(selectedNodes[0], selectedNodes[1], network);
+          System.out.println(isConnected);
+      }
+    }
+  }  
+  
+  public class hasPathAvoidingAction extends CytoscapeAction {
+
+    CyNetwork network;
+    CyNetworkView view;    
+
+    public hasPathAvoidingAction(plugin myPlugin) {
+      super("Has path avoiding");
+      setPreferredMenu("Plugins.Failure Points");
+    }
+
+    public void actionPerformed(ActionEvent e) {
+      view = Cytoscape.getCurrentNetworkView();
+      network = Cytoscape.getCurrentNetwork();
+      
+      if (view.getSelectedNodeIndices().length != 2) {
+          JOptionPane.showMessageDialog(view.getComponent(), "Please select 2 nodes");
+      } else {
+          int selectedNodes[] = view.getSelectedNodeIndices();
+          //calling the method hasPath (implemented below) to see if there is a path
+          //between selectedNode[0] and selectedNodes[1], then print the result to the
+          //command line
+          boolean isConnected = util.hasPathAvoiding(selectedNodes[0], selectedNodes[1], -1, network);
+          System.out.println(isConnected);
+      }
+    }
   }  
   
 
--- src/failurePoints/util.java
+++ src/failurePoints/util.java
@@ -14,6 +14,76 @@ import java.util.Vector;
  */
 public class util {
 
+  public static boolean hasPathAvoiding(int source, int target, int avoid, CyNetwork net) {
+
+    //the hasPath boolean is created with inital value false
+    boolean hasPath = false;
+
+    HashMap visited = new HashMap();
+    visited.put(avoid, 1);
+    
+    //The following wector contains the nodes that we still have to process
+    Vector<Integer> toConsider = new Vector<Integer>();
+    //The only node to consider at the beginning is the source node
+    toConsider.add(source);
+
+    //While we have nodes to consider and we have not visited all the nodes and we
+    //have not found the target node on the way...
+    while (toConsider.size() > 0 && !hasPath) {
+      //Retrieve the current node from the nodes to consider
+      int curNode = toConsider.elementAt(0);
+      //Remove the current node from the nodes to consider
+      toConsider.removeElementAt(0);
+      //If the node was not visited before
+      //then let's mark it as visited
+      //(i.e. add an entry to the visited hash map).
+      if (!visited.containsKey(curNode)) {
+        visited.put(curNode, 1);
+        //If the current node is the target node then we are done
+        //set the result to current node and let the loop know that we found the target
+        if (curNode == target) {
+          //result = curNode;
+          hasPath = true;
+        } else {
+          //We have to retrieve all the children nodes of curNode and
+          //add them to the list of nodes to consider
+
+          //Let's get all the outgoing edges from the curNode
+          int kids[] = net.getAdjacentEdgeIndicesArray(curNode, false, true, true);
+          //Let's loop through all the outgoing edges to get the children
+          //of the curNode
+          for (int i = 0; i < kids.length; i++) {
+            //The neighbor node of the kids[i]-th edge is going to be the node that is the target
+            //of the kids[i]-th edge
+            int neighborNodeID = net.getEdgeTargetIndex(kids[i]);
+            if (neighborNodeID == curNode) {
+              neighborNodeID = net.getEdgeSourceIndex(kids[i]);
+            }
+            //Has the neighbor node been visited yet ??
+            if (!visited.containsKey(neighborNodeID)) {
+              //If not, add it as a child of the currentNode
+              //curNode.addChild(neighborNodeID);
+              //Create a pathnode object for it
+              //pathNode nextNode = new pathNode(neighborNodeID, curNode.ID, path.size());
+              //and add to the nodes to be considered
+              toConsider.add(neighborNodeID);
+            }
+
+          }
+        }
+
+      }
+    }
+
+    //Some cleaning up if the structure is not empty.
+    toConsider.clear();
+    //Finally let's return the result
+
+
+    return hasPath;
+  }
+  
+  
   /*
    * This is a method that returns a boolean value true if there is a path between the source and target nodes, and
    * false if no path can be identified. the method signature is expecting 3 parameters: 1. the ID of the source node
